5294fb20795813fcf52b041ee9a1281884792aac
matstyler <mat.franczuk@gmail.com>
2024-08-20 17:20:45 +0200

    feat: token-list as a package ready to use

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
+export { default as protocolList } from "./protocols/protocolList.json"
 export { default as protocolCustomContent } from "./protocols/protocolCustomContent"
 export type { default as ProtocolText } from "./types/ProtocolText"
+
+export { default as spectraTokens } from "./tokens/spectraTokens.json"
+export { default as erc4626Wrappers } from "./tokens/erc4626Wrappers.json"

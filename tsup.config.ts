1adbfafa1843673bb4d5ddd1c540654502f0b46f
Mat <63294765+matstyler@users.noreply.github.com>
2024-08-23 10:13:36 +0200

    feat: token list as a package (#4)
    * feat: list verification pipeline v1
    
    * fix: path
    
    * fix: path
    
    * fix: path
    
    * fix: path
    
    * fix: path
    
    * fix: schema file name
    
    * fix: extensions schema
    
    * fix: token schema
    
    * fix: missing logos
    
    * feat: platform list validation
    
    * fix: clean platform list schema
    
    * fix: underlying property
    
    * fix: exclude bgURL from required properties
    
    * feat: build as a package
    
    * feat: token-list as a package ready to use
    
    * fix: author

---
+++ tsup.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "tsup"
+
+export default defineConfig({
+    entry: ["src/index.ts"],
+    dts: true, // Generate declaration file (.d.ts)
+    splitting: false,
+    sourcemap: true,
+    clean: true
+})

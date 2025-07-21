13857e6b050afb63091a417b6fafff6d5a88b358
ulydev <ulydev@live.fr>
2025-07-21 14:49:11 +0200

    feat: refactor repository

--- refactor.ts
+++ refactor.ts
@@ -1,24 +1,79 @@
 import fs from "fs"
 
-const replaceTokenLogo = async (token: { chainId: number, address: string, logoURI: string }) => {
-    const needToReplaceLogo = token.logoURI.startsWith("/") && (token.logoURI as string).split("/").length-1 === 3
-        if (needToReplaceLogo) {
+const replaceTokenLogo =
+    async (token: {
+        chainId: number
+        address: string
+        logoURI: string
+    }) => {
+        const needToReplaceLogo =
+            token.logoURI.startsWith(
+                "/",
+            ) &&
+            (
+                token.logoURI as string
+            ).split(
+                "/",
+            )
+                .length -
+                1 ===
+                3
+        if (
+            needToReplaceLogo
+        ) {
             const newURI = `/images/tokens/${token.chainId}/${token.address.toLowerCase()}.${token.logoURI.split(".")[1]}`
-            fs.copyFileSync("./src" + token.logoURI, "./src" + newURI)
-            token.logoURI = newURI
+            fs.copyFileSync(
+                "./src" +
+                    token.logoURI,
+                "./src" +
+                    newURI,
+            )
+            token.logoURI =
+                newURI
         }
+    }
+
+const run = async (
+    tokenListPath: string,
+) => {
+    const tokenList =
+        JSON.parse(
+            fs
+                .readFileSync(
+                    tokenListPath,
+                )
+                .toString(),
+        )
+    tokenList.forEach(
+        (token) => {
+            replaceTokenLogo(
+                token,
+            )
+            if (
+                token
+                    .extensions
+                    ?.vault
+                    ?.logoURI
+            ) {
+                replaceTokenLogo(
+                    token
+                        .extensions
+                        .vault,
+                )
+            }
+        },
+    )
+    fs.writeFileSync(
+        tokenListPath,
+        JSON.stringify(
+            tokenList,
+        ),
+    )
 }
 
-const run = async (tokenListPath: string) => {
-    const tokenList = JSON.parse(fs.readFileSync(tokenListPath).toString())
-    tokenList.forEach(token => {
-        replaceTokenLogo(token)
-        if (token.extensions?.vault?.logoURI) {
-            replaceTokenLogo(token.extensions.vault)
-        }
-    })
-    fs.writeFileSync(tokenListPath, JSON.stringify(tokenList))
-}
-
-run("./src/tokens/spectraTokens.json")
-run("./src/tokens/erc4626Wrappers.json")
+run(
+    "./src/tokens/spectraTokens.json",
+)
+run(
+    "./src/tokens/erc4626Wrappers.json",
+)
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 export { default as protocolList } from "./protocols/protocolList.json"
 export { default as curatorList } from "./curators/curatorList.json"
-export type { default as ProtocolText } from "./types/ProtocolText"
 
-export { default as spectraTokens } from "./tokens/spectraTokens.json"
-export { default as erc4626Wrappers } from "./tokens/erc4626Wrappers.json"
+export { default as spectraTokens } from "./tokens/build.json"
+export { default as erc4626Wrappers } from "./wrappers/build.json"

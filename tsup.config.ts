687398fea87ff34e7d6d49e2167617d39769cdf6
ulydev <ulydev@live.fr>
2025-07-21 14:59:52 +0200

    fix: build

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from "tsup"
 
-export default defineConfig({
-    entry: ["src/index.ts"],
-    dts: true, // Generate declaration file (.d.ts)
-    splitting: false,
-    sourcemap: true,
-    clean: true
-})
+export default defineConfig(
+    {
+        entry: [
+            "src/index.ts",
+        ],
+        dts: true, // Generate declaration file (.d.ts)
+        splitting:
+            false,
+        sourcemap:
+            true,
+        clean: true,
+    },
+)
